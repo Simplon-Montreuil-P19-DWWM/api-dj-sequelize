1a17321ad23bda2ac906d526fda466e3e1d5f8e8
puthpiseth <puthpiseth_tun@yahoo.com>
2021-06-23 14:35:07 +0200

    Create db tables

--- src/models/djmusicalgenres.js
+++ src/models/djmusicalgenres.js
@@ -1,37 +1,37 @@
-// 'use strict';
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class DjMusicalgenres extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       this.belongsTo(models.Djs, {foreingKey: "dj_id"});
-//       this.belongsTo(models.Musicalgenres, {foreingKey: "musicalgenre_id"});
-//     }
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DjMusicalgenres extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Djs, {foreingKey: "dj_id"});
+      this.belongsTo(models.Musicalgenres, {foreingKey: "musicalgenre_id"});
+    }
     
-//   };
-//   DjMusicalgenres.init({
-//     id: {
-//       type: DataTypes.UUID,
-//       allowNull: false,
-//       primaryKey: true,
-//     },
-//     dj_id: {
-//       type: DataTypes.UUID,
-//       allowNull: false,
-//     },
-//     musicalgenre_id: {
-//       type: DataTypes.UUID,
-//       allowNull: false,
-//     },
-//   }, {
-//     sequelize,
-//     modelName: 'DjMusicalgenres',
-//   });
-//   return DjMusicalgenres;
-// };
+  };
+  DjMusicalgenres.init({
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    dj_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    musicalgenre_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'DjMusicalgenres',
+  });
+  return DjMusicalgenres;
+};
